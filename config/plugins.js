053a8f91766021c3504e785fe99b234607061dd1
Paula <mpvillamarin@gmail.com>
2025-01-02 11:47:29 -0500

    Prueba Cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
+      },
+      actionOptions: {
+        upload: {
+          folder: 'strapi_uploads', // Carpeta específica en Cloudinary (opcional)
+        },
+        delete: {},
+      },
+    },
+  });
+  
